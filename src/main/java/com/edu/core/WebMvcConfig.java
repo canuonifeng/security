4de3b28cf832104adbaf8b1fb65d00d53c6c7b58
wangxibin <wangxibin@howzhi.com>
2017-07-19 10:15:02 +0800

    增加 interceptor

---
+++ src/main/java/com/edu/core/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.edu.core;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+	@Autowired
+	TimeCostInterceptor timeCostInterceptor;
+
+	@Override
+	  public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(timeCostInterceptor);
+	  }
+}
