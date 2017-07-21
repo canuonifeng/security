394285b59ae5d2f44f8741f5b998d1f51e07cb04
wangxibin <wangxibin@howzhi.com>
2017-07-21 16:46:35 +0800

    异步事件

---
+++ src/main/java/com/edu/biz/event/listener/CreateUserListener.java
@@ -0,0 +1,24 @@
+package com.edu.biz.event.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import com.edu.biz.event.CreateUserEvent;
+import com.edu.biz.security.entity.User;
+
+@Component
+public class CreateUserListener implements ApplicationListener<CreateUserEvent> {
+	public final static Logger logger = LoggerFactory
+			.getLogger(CreateUserListener.class);
+	@Async
+	@Override
+	public void onApplicationEvent(CreateUserEvent event) {
+	User user = (User) event.getSource();
+	logger.debug(user.getNickname()+" 用户被创建，"+user);
+		
+	}
+
+}
