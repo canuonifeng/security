32d0380fd42fda6fe13f3eb6109ff03d796891c8
wangxibin <wangxibin@howzhi.com>
2017-07-21 16:15:14 +0800

    为毛不行

--- src/main/java/com/edu/core/exception/handler/ServiceExceptionHandler.java
+++ src/main/java/com/edu/core/exception/handler/ServiceExceptionHandler.java
@@ -5,31 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.edu.core.exception.ServiceException;
 
 @ControllerAdvice
-public class ServiceExceptionHandler extends ResponseEntityExceptionHandler {
-
-	@ExceptionHandler(Throwable.class)
-	@ResponseBody
-	ErrorInfo handleControllerException(HttpServletRequest req, Throwable ex) {
-
-		if (ex instanceof ServiceException) {
-			return serviceExceptionHandler(req, (ServiceException) ex);
-		} else {
-			ErrorInfo<String> err = new ErrorInfo<>();
-	        err.setMessage("服务器异常");
-	        err.setStatus("500");
-	        err.setData(ex.getMessage());
-	        return err;
-		}
-	}
+public class ServiceExceptionHandler  {
 
 	@ExceptionHandler(value = ServiceException.class)
 	@ResponseBody
-	public ErrorInfo serviceExceptionHandler(HttpServletRequest req, ServiceException e) {
+	public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, ServiceException e) {
 		ErrorInfo<String> err = new ErrorInfo<>();
 		err.setMessage(e.getMessage());
 		err.setStatus(e.getCode());
