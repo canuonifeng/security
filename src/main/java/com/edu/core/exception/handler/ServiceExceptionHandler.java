9d7d668a878a9baa802c66ef0abebf7f8fbc30c8
wangxibin <wangxibin@howzhi.com>
2017-07-21 15:12:26 +0800

    fix

--- src/main/java/com/edu/core/exception/handler/ServiceExceptionHandler.java
+++ src/main/java/com/edu/core/exception/handler/ServiceExceptionHandler.java
@@ -5,18 +5,35 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.edu.core.exception.ServiceException;
 
 @ControllerAdvice
-public class ServiceExceptionHandler {
+public class ServiceExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Throwable.class)
+	@ResponseBody
+	ErrorInfo handleControllerException(HttpServletRequest req, Throwable ex) {
+
+		if (ex instanceof ServiceException) {
+			return serviceExceptionHandler(req, (ServiceException) ex);
+		} else {
+			ErrorInfo<String> err = new ErrorInfo<>();
+	        err.setMessage("服务器异常");
+	        err.setStatus("500");
+	        err.setData(ex.getMessage());
+	        return err;
+		}
+	}
+
 	@ExceptionHandler(value = ServiceException.class)
-    @ResponseBody
-    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, ServiceException e) throws Exception {
-        ErrorInfo<String> err = new ErrorInfo<>();
-        err.setMessage(e.getMessage());
-        err.setStatus(e.getCode());
-        err.setData(e.getMessage());
-        return err;
-    }
+	@ResponseBody
+	public ErrorInfo serviceExceptionHandler(HttpServletRequest req, ServiceException e) {
+		ErrorInfo<String> err = new ErrorInfo<>();
+		err.setMessage(e.getMessage());
+		err.setStatus(e.getCode());
+		err.setData(e.getMessage());
+		return err;
+	}
 }
