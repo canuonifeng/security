01555e7906df483454d25bacc4c344be396bf067
Administrator <Administrator@PC-20170328LZDT>
2017-07-22 19:33:46 +0800

    add org

--- src/main/java/com/edu/core/WebSecurityConfig.java
+++ src/main/java/com/edu/core/WebSecurityConfig.java
@@ -25,8 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-		.anyRequest().authenticated()
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated()
+		.and().formLogin().loginProcessingUrl("/loginCheck")
 		.and().httpBasic()
 		.authenticationEntryPoint(authEntryPoint);
 	}
