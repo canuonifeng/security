4efaa799493817420d14edee64b2840540c43626
nifeng <nifeng@howzhi.com>
2017-07-27 14:27:53 +0800

    fix user

--- src/main/java/com/edu/controller/UserController.java
+++ src/main/java/com/edu/controller/UserController.java
@@ -22,8 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.edu.biz.security.entity.User;
 import com.edu.biz.security.service.UserService;
 
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
@@ -44,12 +42,6 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.POST)
 	@PreAuthorize("hasPermission('user', 'add')")
 	@ApiOperation(value = "新增用户", notes = "根据提交的数据创建新用户")
-	@ApiImplicitParams({
-		@ApiImplicitParam(name = "username", value = "登录名", required = true, dataType = "String"),
-		@ApiImplicitParam(name = "password", value = "密码", required = true, dataType = "String"),
-		@ApiImplicitParam(name = "email", value = "邮箱", dataType = "String"),
-		@ApiImplicitParam(name = "nickname", value = "昵称", dataType = "String"),
-	})
 	public User add(@Valid @RequestBody User user) {
 		return userService.createUser(user);
 	}
@@ -57,12 +49,6 @@ public class UserController {
 	@RequestMapping(path = "/{id}", method = RequestMethod.PUT)
 	@PreAuthorize("hasPermission('user', 'edit')")
 	@ApiOperation(value = "编辑用户信息")
-	@ApiImplicitParams({
-		@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
-		@ApiImplicitParam(name = "username", value = "登录名", required = true, dataType = "String"),
-		@ApiImplicitParam(name = "email", value = "邮箱", dataType = "String"),
-		@ApiImplicitParam(name = "nickname", value = "昵称", dataType = "String"),
-	})
 	public User edit(@PathVariable Long id, @RequestBody User user) {
 		user.setId(id);
 		return userService.updateUser(user);
@@ -71,7 +57,6 @@ public class UserController {
 	@RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
 	@PreAuthorize("hasPermission('user', 'delete')")
 	@ApiOperation(value = "删除用户", notes = "根据url的id来指定删除对象")
-	@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
 	public void delete(@PathVariable Long id) {
 		userService.deleteUser(id);
 	}
@@ -93,13 +78,9 @@ public class UserController {
 	@RequestMapping(path = "/password", method = RequestMethod.PUT)
 	@PreAuthorize("isAuthenticated()")
 	@ApiOperation(value = "设置当前用户新密码")
-	@ApiImplicitParams({
-		@ApiImplicitParam(name = "oldPassword", value = "老密码", required = true, dataType = "String"),
-		@ApiImplicitParam(name = "newPassword", value = "新密码", required = true, dataType = "String"),
-	})
-	public boolean setNewPassword(@RequestBody Map<String,String> map) {
+	public boolean setNewPassword(@RequestBody String oldPassword, @RequestBody String newPassword) {
 		User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		userService.setNewPassword(user.getId(), map.get("oldPassword"), map.get("newPassword"));
+		userService.setNewPassword(user.getId(), oldPassword, newPassword);
 		return true;
 	}
 }
