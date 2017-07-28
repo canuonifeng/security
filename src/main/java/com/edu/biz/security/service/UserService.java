1a5675f111d9547b6ca5b3cc5d0627ed3a895d4d
wangxibin <wangxibin@howzhi.com>
2017-07-28 11:52:25 +0800

    用户名校验

--- src/main/java/com/edu/biz/security/service/UserService.java
+++ src/main/java/com/edu/biz/security/service/UserService.java
@@ -15,6 +15,8 @@ public interface UserService {
 	public User createUser(User user);
 
 	public User updateUser(User user);
+	
+	public Boolean checkUserName(String userName,Long userId);
 
 	public boolean deleteUser(Long id);
 	
--- src/main/java/com/edu/controller/UserController.java
+++ src/main/java/com/edu/controller/UserController.java
@@ -41,6 +41,12 @@ public class UserController {
 			@PageableDefault(value = 15, sort = { "id" }, direction = Sort.Direction.DESC) Pageable pageable) {
 		return userService.searchUsers(conditions, pageable);
 	}
+	
+	@RequestMapping(path = "/check_username",method = RequestMethod. GET)
+	@ApiOperation(value = "检查用户名是否重复", notes = "根据用户名检查是否重复")
+	public Boolean checkUserName(String userName,  Long userId){
+		 return userService.checkUserName(userName, userId);
+	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	@PreAuthorize("hasPermission('user', 'add')")
