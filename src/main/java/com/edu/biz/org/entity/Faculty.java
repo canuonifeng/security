ee62937508bcabfea11e57857826f4b13dcf28ca
zhaoyao <zhaoyao@howzhi.com>
2017-07-31 10:14:03 +0800

    fix

--- src/main/java/com/edu/biz/org/entity/Faculty.java
+++ src/main/java/com/edu/biz/org/entity/Faculty.java
@@ -1,21 +1,15 @@
 package com.edu.biz.org.entity;
 
-import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 
 import com.edu.biz.base.BaseEntity;
-import com.edu.biz.security.entity.Organization;
 
 @Entity
 public class Faculty extends BaseEntity {
 
 	private String code;
 	private String name; 
-	
-//	@OneToMany(mappedBy="faculty")
-//	private Set<Organization> organization;
 	
 	public String getCode() {
 		return code;
@@ -32,12 +26,4 @@ public class Faculty extends BaseEntity {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-//	public Set<Organization> getOrganization() {
-//		return organization;
-//	}
-//
-//	public void setOrganization(Set<Organization> organization) {
-//		this.organization = organization;
-//	}
 }
