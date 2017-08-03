c2dd1ca3786f5a1a584be2a2822a9afa24bbe400
wangxibin <wangxibin@howzhi.com>
2017-08-03 19:55:21 +0800

    专业，班级表结构

---
+++ src/main/resources/migrations/V1.0.6__create_major_classroom.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `major` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `code` varchar(64) NOT NULL COMMENT '专业编号',
+  `name` varchar(64) NOT NULL COMMENT '名称',
+  `faculty_id` int(10) UNSIGNED NOT NULL COMMENT '院系ID',
+  `created_time` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='专业表';
+
+CREATE TABLE IF NOT EXISTS `classroom` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `code` varchar(64) NOT NULL COMMENT '编号',
+  `name` varchar(64) NOT NULL COMMENT '名称',
+  `major_id` int(10) UNSIGNED NOT NULL COMMENT '专业ID',
+  `created_time` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='班级表';
