b91de85f489c6b09e3da4cba12a350e9b744833d
wangxibin <wangxibin@howzhi.com>
2017-08-07 09:03:53 +0800

    fix

--- src/main/resources/migrations/V1.0.8__create_student_table.sql
+++ src/main/resources/migrations/V1.0.8__create_student_table.sql
@@ -2,13 +2,13 @@ CREATE TABLE IF NOT EXISTS `student` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `no` varchar(64) NOT NULL COMMENT '学号',
   `name` varchar(64) NOT NULL COMMENT '姓名',
-  `gender` ENUM('male','female','secret') NOT NULL DEFAULT 'secret' COMMENT '性别'，
+  `gender` ENUM('male','female','secret') NOT NULL DEFAULT 'secret' COMMENT '性别',
   `year_month` varchar(12) NOT NULL COMMENT '入学年月',
   `grade` varchar(4) NOT NULL COMMENT '年级',
   `major_id` int(10) UNSIGNED NOT NULL COMMENT '专业ID',
   `classroom_id` int(10) UNSIGNED COMMENT '班级ID',
-  `status` ENUM('enable','disable','delete','changing') NOT NULL DEFAULT 'enable' COMMENT '状态，正常，禁用，删除，异动中，退学，休学，开除，劝退'
-  `from` ENUM('alone','unified','selftaught') NOT NULL DEFAULT 'higher' COMMENT '学生来源，单招、统考、自考'，
+  `status` ENUM('enable','disable','delete','changing') NOT NULL DEFAULT 'enable' COMMENT '状态，正常，禁用，删除，异动中，退学，休学，开除，劝退',
+  `from` ENUM('alone','unified','selftaught') NOT NULL DEFAULT 'unified' COMMENT '学生来源，单招、统考、自考',
   `native_place` varchar(64) COMMENT '籍贯',
   `idcard` varchar(32) COMMENT '身份证号',
   `remark` text COMMENT '备注',
