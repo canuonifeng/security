cb29fd1fe1937ecc9d56ea77be12d4c9b5a520d9
zhaoyao <zhaoyao@howzhi.com>
2017-08-09 09:57:45 +0800

    修改关联

--- src/main/java/com/edu/biz/schoolroll/dao/ClassroomMemberDao.java
+++
@@ -1,8 +0,0 @@
-package com.edu.biz.schoolroll.dao;
-
-import com.edu.biz.base.BaseDao;
-import com.edu.biz.schoolroll.entity.ClassroomMember;
-
-public interface ClassroomMemberDao extends BaseDao<ClassroomMember> {
-	
-}
--- src/main/java/com/edu/biz/schoolroll/service/ClassroomService.java
+++ src/main/java/com/edu/biz/schoolroll/service/ClassroomService.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.edu.biz.schoolroll.entity.Classroom;
-import com.edu.biz.schoolroll.entity.ClassroomMember;
 
 public interface ClassroomService {
 	
@@ -25,14 +24,4 @@ public interface ClassroomService {
 	public Page<Classroom> searchClassroom(Map<String, Object> conditions, Pageable pageable);
 	
 	public Long countClassroom(Map<String, Object> conditions);
-	
-	public ClassroomMember createClassroomMember(ClassroomMember classroomMember);
-	
-	public ClassroomMember getClassroomMember(Long id);
-	
-	public Boolean deleteClassroomMember(Long id);
-	
-	public ClassroomMember updateClassroomMember(ClassroomMember classroomMember);
-	
-	public Long countClassroomMember(Map<String, Object> conditions);
 }
