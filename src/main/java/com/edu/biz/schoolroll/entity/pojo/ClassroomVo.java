2c2cd0ffaa766d552173def6ad77d8871a5a6498
zhaoyao <zhaoyao@howzhi.com>
2017-08-14 19:59:43 +0800

    classroom vo

--- src/main/java/com/edu/biz/schoolroll/entity/pojo/ClassroomVo.java
+++ src/main/java/com/edu/biz/schoolroll/entity/pojo/ClassroomVo.java
@@ -3,8 +3,10 @@ package com.edu.biz.schoolroll.entity.pojo;
 import com.edu.biz.schoolroll.entity.Classroom;
 
 public class ClassroomVo extends Classroom {
-	
+
 	private int studentNum;
+	private int femaleNum;
+	private int maleNum;
 
 	public int getStudentNum() {
 		return studentNum;
@@ -13,5 +15,21 @@ public class ClassroomVo extends Classroom {
 	public void setStudentNum(int studentNum) {
 		this.studentNum = studentNum;
 	}
-	
+
+	public int getFemaleNum() {
+		return femaleNum;
+	}
+
+	public void setFemaleNum(int femaleNum) {
+		this.femaleNum = femaleNum;
+	}
+
+	public int getMaleNum() {
+		return maleNum;
+	}
+
+	public void setMaleNum(int maleNum) {
+		this.maleNum = maleNum;
+	}
+
 }
--- src/main/java/com/edu/biz/schoolroll/entity/pojo/MajorVo.java
+++ src/main/java/com/edu/biz/schoolroll/entity/pojo/MajorVo.java
@@ -3,5 +3,49 @@ package com.edu.biz.schoolroll.entity.pojo;
 import com.edu.biz.schoolroll.entity.Major;
 
 public class MajorVo extends Major {
-	
+	private int studentNum;
+	private int femaleNum;
+	private int maleNum;
+	private int unAssginFemalNum;
+	private int unAssginMaleNum;
+
+	public int getStudentNum() {
+		return studentNum;
+	}
+
+	public void setStudentNum(int studentNum) {
+		this.studentNum = studentNum;
+	}
+
+	public int getFemaleNum() {
+		return femaleNum;
+	}
+
+	public void setFemaleNum(int femaleNum) {
+		this.femaleNum = femaleNum;
+	}
+
+	public int getMaleNum() {
+		return maleNum;
+	}
+
+	public void setMaleNum(int maleNum) {
+		this.maleNum = maleNum;
+	}
+
+	public int getUnAssginFemalNum() {
+		return unAssginFemalNum;
+	}
+
+	public void setUnAssginFemalNum(int unAssginFemalNum) {
+		this.unAssginFemalNum = unAssginFemalNum;
+	}
+
+	public int getUnAssginMaleNum() {
+		return unAssginMaleNum;
+	}
+
+	public void setUnAssginMaleNum(int unAssginMaleNum) {
+		this.unAssginMaleNum = unAssginMaleNum;
+	}
 }
