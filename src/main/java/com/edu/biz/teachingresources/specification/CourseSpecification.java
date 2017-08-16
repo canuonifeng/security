0f660e52f7562ad24689ffa37a3b45ee07b572d2
zhaoyao <zhaoyao@howzhi.com>
2017-08-16 19:15:41 +0800

    fix

---
+++ src/main/java/com/edu/biz/teachingresources/specification/CourseSpecification.java
@@ -0,0 +1,40 @@
+package com.edu.biz.teachingresources.specification;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.edu.biz.teachingresources.entity.Course;
+import com.edu.biz.teachingresources.entity.Teacher;
+
+public class CourseSpecification implements Specification<Course> {
+	private Map<String, Object> conditions;
+	
+	public CourseSpecification(Map<String, Object> conditions) {
+		this.conditions = conditions;
+	}
+	
+	@Override
+	public Predicate toPredicate(Root<Course> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+		List<Predicate> list = new ArrayList<Predicate>();
+
+		if (null != conditions) {
+			if (conditions.containsKey("name")) {
+				list.add(cb.like(root.get("name"), "%"+this.conditions.get("name")+"%"));
+			}
+			if (conditions.containsKey("code")) {
+				list.add(cb.equal(root.get("code"), this.conditions.get("code")));
+			}
+		}
+
+		Predicate[] p = new Predicate[list.size()];
+		return cb.and(list.toArray(p));
+	}
+}
