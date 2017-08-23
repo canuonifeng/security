f6b087839314cf91a7253af58d30250eac8375bc
zhaoyao <zhaoyao@howzhi.com>
2017-08-23 08:39:03 +0800

    添加权限

--- src/main/java/com/edu/controller/OrgController.java
+++ src/main/java/com/edu/controller/OrgController.java
@@ -73,6 +73,8 @@ public class OrgController extends BaseController<Organization> {
 	}
 	
 	@RequestMapping(path = "/findOrgs", method = RequestMethod.GET)
+	@PreAuthorize("hasPermission('org', 'get')")
+	@JsonView(OrgJsonViews.CascadeChildren.class)
 	public List<Organization> findMajors(@RequestParam Map<String, Object> conditions) {
 		List<Organization> list = orgService.findOrgs(conditions);
 		
