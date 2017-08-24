6e9255e2b55f354248c807e5cdea33448c2b8c72
zhaoyao <zhaoyao@howzhi.com>
2017-08-24 11:45:15 +0800

    增加字段

--- src/main/resources/migrations/V1.0.28__add_teaching_program_course_fields.sql
+++ src/main/resources/migrations/V1.0.28__add_teaching_program_course_fields.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `teaching_program_course` ADD `term` INT(10) NULL COMMENT '所属学期' AFTER `theory_period`;
 ALTER TABLE `teaching_program_course` ADD `term_code` INT(10) NULL COMMENT '所属学期编码' AFTER `term`;
+ALTER TABLE `teaching_program_course` ADD `week_period` INT(10) NULL COMMENT '周学时' AFTER `term_code`;
