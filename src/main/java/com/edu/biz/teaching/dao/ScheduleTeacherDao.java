477a22e4767fa3a29d6522d40ed37623afbe0777
zhaoyao <zhaoyao@howzhi.com>
2017-09-12 08:58:52 +0800

    修改方法

--- src/main/java/com/edu/biz/teaching/dao/ScheduleTeacherDao.java
+++ src/main/java/com/edu/biz/teaching/dao/ScheduleTeacherDao.java
@@ -1,11 +1,8 @@
 package com.edu.biz.teaching.dao;
 
-import org.springframework.data.jpa.repository.Modifying;
-
 import com.edu.biz.base.BaseDao;
 import com.edu.biz.teaching.entity.ScheduleTeacher;
 
 public interface ScheduleTeacherDao extends BaseDao<ScheduleTeacher> {
-	@Modifying
 	public void deleteByClassScheduleId(Long scheduleId);
 }
