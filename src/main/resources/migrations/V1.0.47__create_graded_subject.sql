2edd0781b11201f5bb303c65e81a6136291b0bca
zhaoyao <zhaoyao@howzhi.com>
2017-11-13 16:46:15 +0800

    添加所属年纪

--- src/main/resources/migrations/V1.0.47__create_graded_subject.sql
+++ src/main/resources/migrations/V1.0.47__create_graded_subject.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `graded_subject` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `name` varchar(255)  NOT NULL COMMENT '分层科目名字',
   `major_id` int(10) UNSIGNED NOT NULL COMMENT '所属专业id',
+  `grade` varchar(4) NOT NULL COMMENT '所属年级',
   `created_time` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
   `updated_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后更新时间',
   PRIMARY KEY (`id`)
