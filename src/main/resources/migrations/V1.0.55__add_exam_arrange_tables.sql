d397890dd94b8d1829dea21cd2e502e39b17f54e
xiexiyu <xiexiyu@howzhi.com>
2017-11-29 15:53:23 +0800

    注释修改

--- src/main/resources/migrations/V1.0.55__add_exam_arrange_tables.sql
+++ src/main/resources/migrations/V1.0.55__add_exam_arrange_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `exam_arrange` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '安排考试ID',
   `course_id` int(10) UNSIGNED COMMENT '课程ID',
   `exam_time` varchar(32) COMMENT '考试时间',
-  `faculty_id` int(10) UNSIGNED COMMENT '课程ID',
+  `faculty_id` int(10) UNSIGNED COMMENT '院系ID',
   `grade` varchar(24) COMMENT '年级',
   `term_code` varchar(32) COMMENT '学期代码',
   `created_time` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `exam_arrange` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `exam_arrange_room` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '排考关联考场ID',
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '考试房间ID',
   `exam_arrange_id` int(10) UNSIGNED COMMENT '排考ID',
   `building_room_id` int(10) UNSIGNED COMMENT '考场ID',
   `created_time` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS `exam_arrange_room` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `exam_arrange_room_teacher` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '排考考场关联教师ID',
-  `exam_arrange_room_id` int(10) UNSIGNED COMMENT '考场ID',
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '考试房间关联教师ID',
+  `exam_arrange_room_id` int(10) UNSIGNED COMMENT '考试房间ID',
   `teacher_id` int(10) UNSIGNED COMMENT '教师ID',
   `created_time` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
   `updated_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后更新时间',
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS `exam_arrange_room_teacher` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `exam_arrange_room_member` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '安排考试和教师关联ID',
-  `exam_arrange_room_id` int(10) UNSIGNED COMMENT '排考关联考场ID',
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '考试房间和考试学员关联ID',
+  `exam_arrange_room_id` int(10) UNSIGNED COMMENT '考试房间ID',
   `student_id` int(10) UNSIGNED COMMENT '考试学员ID',
   `created_time` TIMESTAMP NULL DEFAULT NULL COMMENT '创建时间',
   `updated_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后更新时间',
