902c6fbe560a13d3c14cc174e32b891b111bb001
zhaoyao <zhaoyao@howzhi.com>
2017-11-30 15:50:43 +0800

    排考课程搜索

--- src/main/java/com/edu/biz/exam/service/ExamArrangeService.java
+++ src/main/java/com/edu/biz/exam/service/ExamArrangeService.java
@@ -1,7 +1,16 @@
 package com.edu.biz.exam.service;
 
+import java.util.List;
+import java.util.Map;
+
+import com.edu.biz.exam.entity.ExamAboutFacultyAndGradeAndTestWay;
 import com.edu.biz.exam.entity.ExamArrange;
+import com.edu.biz.teachingres.entity.Course;
 
 public interface ExamArrangeService {
 	public void createExamArrange(ExamArrange examArrange);
+
+	public List<ExamAboutFacultyAndGradeAndTestWay> getExamList(Map<String, Object> conditions);
+
+	public List<Course> findExamArrangeCourses(Map<String, Object> conditions);
 }
--- src/main/java/com/edu/controller/ExamArrangeController.java
+++ src/main/java/com/edu/controller/ExamArrangeController.java
@@ -1,7 +1,5 @@
 package com.edu.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,12 +15,7 @@ import com.edu.biz.exam.entity.ExamAboutFacultyAndGradeAndTestWay;
 import com.edu.biz.exam.entity.ExamArrange;
 import com.edu.biz.exam.service.ExamArrangeService;
 import com.edu.biz.org.entity.Faculty;
-import com.edu.biz.org.service.FacultyService;
-import com.edu.biz.schoolroll.entity.Classroom;
-import com.edu.biz.schoolroll.service.ClassroomService;
-import com.edu.biz.teaching.entity.Term;
-import com.edu.biz.teaching.service.ProgramService;
-import com.edu.biz.teaching.service.TermService;
+import com.edu.biz.teachingres.entity.Course;
 
 import io.swagger.annotations.Api;
 
@@ -31,37 +24,12 @@ import io.swagger.annotations.Api;
 @Api("排考")
 public class ExamArrangeController extends BaseController<Faculty> {
 	@Autowired
-	private FacultyService facultyService;
-	@Autowired
-	private ClassroomService classroomService;
-	@Autowired
-	private ProgramService programService;
-	@Autowired
-	private TermService TermService;
-	@Autowired
 	private ExamArrangeService examArrangeService;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	@PreAuthorize("hasPermission('examArrange', 'get')")
 	public List<ExamAboutFacultyAndGradeAndTestWay> examList(@RequestParam Map<String, Object> conditions) {
-		List<ExamAboutFacultyAndGradeAndTestWay> list = new ArrayList<>();
-		List<Faculty> facultys = facultyService.findFacultys(conditions);
-		for (Faculty faculty:facultys) {
-			Map<String, Object> classroomMap = new HashMap<>();
-			classroomMap.put("grade", conditions.get("grade"));
-			classroomMap.put("facultyId", faculty.getId());
-			Long classroomCount = classroomService.countClassroom(classroomMap);
-			Term currenTerm = TermService.getTermByCurrent(1);
-			int courseCount = programService.countWrittenProgramCourses(conditions.get("grade").toString(), faculty.getId(), "written", currenTerm.getCode());
-			ExamAboutFacultyAndGradeAndTestWay examList = new ExamAboutFacultyAndGradeAndTestWay();
-			examList.setFaculty(faculty);
-			examList.setGrade(conditions.get("grade").toString());
-			examList.setClassNumber(Integer.valueOf(classroomCount.toString()));
-			examList.setExamNumber(courseCount);
-			list.add(examList);
-		}
-		
-		return list;
+		return examArrangeService.getExamList(conditions);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -70,5 +38,11 @@ public class ExamArrangeController extends BaseController<Faculty> {
 		for (ExamArrange examArrang:examArranges) {
 			examArrangeService.createExamArrange(examArrang);
 		}
+	}
+	
+	@RequestMapping(path = "/courses", method = RequestMethod.GET)
+	@PreAuthorize("hasPermission('examArrange', 'get')")
+	public List<Course> get(@RequestParam Map<String, Object> conditions) {
+		return examArrangeService.findExamArrangeCourses(conditions);
 	}
 }
