c389ca5184edcc86671caf5b88e3767a33aec068
zhaoyao <zhaoyao@howzhi.com>
2017-12-04 14:44:25 +0800

    成绩设置初始化

--- src/main/resources/migrations/V1.0.57__create_final_grade_part_table.sql
+++ src/main/resources/migrations/V1.0.57__create_final_grade_part_table.sql
@@ -6,6 +6,12 @@ CREATE TABLE IF NOT EXISTS `final_grade_part` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `final_grade_part` (`name`,`created_time`,`updated_time`) VALUES
+('平时成绩', now(), now()),
+('考试成绩', now(), now()),
+('实践成绩', now(), now()),
+('考勤', now(), now());
+
 CREATE TABLE IF NOT EXISTS `final_grade_part_course` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '课程成绩id',
   `faculty_id` int(10) UNSIGNED NOT NULL COMMENT '所属院系id',
