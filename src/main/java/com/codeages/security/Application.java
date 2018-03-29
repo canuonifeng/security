d281235eaf34c3983e1dfef001d5c18919f4e134
nifeng <nifeng@howzhi.com>
2018-03-29 13:55:53 +0800

    delete properties listener

--- src/main/java/com/codeages/security/Application.java
+++ src/main/java/com/codeages/security/Application.java
@@ -3,13 +3,11 @@ package com.codeages.security;
 import org.springframework.boot.SpringApplication;
 
 import com.codeages.framework.FrameworkApplication;
-import com.codeages.framework.property.PropertiesListener;
 
     
 public class Application extends FrameworkApplication{
 	public static void main(String[] args) throws Exception {
 		SpringApplication application = new SpringApplication(Application.class);
-		application.addListeners(new PropertiesListener("classpath: config/application.properties"));
 		application.run(args);
 	}
 }
