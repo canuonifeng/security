8d093d02dc0a11e9d9ea1902cb1284f4806f5e13
nifeng <nifeng@howzhi.com>
2018-03-29 11:55:13 +0800

    delete application.properties

--- src/main/java/com/codeages/security/Application.java
+++ src/main/java/com/codeages/security/Application.java
@@ -3,10 +3,13 @@ package com.codeages.security;
 import org.springframework.boot.SpringApplication;
 
 import com.codeages.framework.FrameworkApplication;
+import com.codeages.framework.property.PropertiesListener;
 
     
 public class Application extends FrameworkApplication{
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		application.addListeners(new PropertiesListener("classpath: config/application.properties"));
+		application.run(args);
 	}
 }
