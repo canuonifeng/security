ac5bde5199a9171c4a501e01ccf801483f1b7e3a
nifeng <nifeng@howzhi.com>
2018-05-18 17:32:18 +0800

    security config

--- src/main/java/com/codeages/security/config/SecurityConfig.java
+++ src/main/java/com/codeages/security/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.codeages.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.codeages.framework.authentication.AuthenticationProvider;
 import com.codeages.framework.config.AuthenticationConfig;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends AuthenticationConfig {
 	
 	@Autowired
